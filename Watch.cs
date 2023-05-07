ed472f70de71d7a950673b1842437d2f00d12031
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-05-07 12:37:04 -0700

    Initial commit

---
+++ Watch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_Users_with_Objects
+{
+    abstract class Watch
+    { 
+            public string Brand { get; set; }
+            public string Model { get; set; }
+            public double Price { get; set; }
+
+            public Watch(string brand, string model, double price)
+            {
+                Brand = brand;
+                Model = model;
+                Price = price;
+            }
+
+            public override string ToString()
+            {
+                return $"{Brand} {Model} (${Price})";
+            }
+        }
+}
