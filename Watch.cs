81edb637105f7dd9944b5e518173ef4063c679f4
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-05-08 01:04:57 -0700

    update

--- Watch.cs
+++ Watch.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace lab_Users_with_Objects
 {
-    abstract class Watch
+    public abstract class Watch
     { 
-            public string Brand { get; set; }
-            public string Model { get; set; }
-            public double Price { get; set; }
+       string Brand { get; set; }
+       string Model { get; set; }
+       double Price { get; set; }
 
-            public Watch(string brand, string model, double price)
-            {
-                Brand = brand;
-                Model = model;
-                Price = price;
-            }
-
-            public override string ToString()
-            {
-                return $"{Brand} {Model} (${Price})";
-            }
+       public Watch(string brand, string model, double price)
+        {
+            Brand = brand;
+            Model = model;
+            Price = price;
         }
+
+        public abstract void SetTime(DateTime time);
+
+        public override string ToString()
+        {
+            return $"{Brand} {Model} (${Price})";
+        }
+    }
 }
